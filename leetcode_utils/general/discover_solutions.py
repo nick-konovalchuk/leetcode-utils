2b16056eb4bbe36ee01fb6eeb6c3b97d9b222a12
Nick Konovalchuk <nikolay.konovalchuk@mail.ru>
2024-01-14 01:27:31 +0300

    fix __file__

--- leetcode_utils/general/discover_solutions.py
+++ leetcode_utils/general/discover_solutions.py
@@ -5,7 +5,7 @@ import os.path as osp
 
 
 def discover_solutions():
-    test_file_path = inspect.currentframe().f_back.f_globals.__file__
+    test_file_path = inspect.currentframe().f_back.f_globals["__file__"]
     solution_paths = glob.glob(osp.abspath(f"{test_file_path}/../solution*.py"))
     solution_names = [osp.basename(path[:-3]) for path in solution_paths]
 
